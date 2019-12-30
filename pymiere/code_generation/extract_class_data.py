d3fbb6d6dfe055b1285c2feb3b974aa43fb750ac
Quentin Masingarbe <q.masingarbe@gmail.com>
2019-12-30 12:00:59 +0100

    Refactor hierachy of lib

--- pymiere/code_generation/extract_class_data.py
+++ pymiere/code_generation/extract_class_data.py
@@ -9,6 +9,6 @@ data = pymiere.eval_script(filepath=filepath_jsxObjectToPythonObject)
 print("Result :")
 pprint(data, depth=3)
 
-utils.write_json_file(r"D:\code\prpro\classData_videoitem_qe.json", data)
+utils.write_json_file(r"D:\code\prpro\classData_globals.json", data)
 
 # todo try using https://github.com/Adobe-CEP/Samples/tree/master/PProPanel/jsx PremierePro.d.ts

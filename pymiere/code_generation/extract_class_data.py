c45f446f9f73df3896fc0783ae2731fccecdd78d
Quentin Masingarbe <q.masingarbe@gmail.com>
2019-12-30 19:33:17 +0100

    Support for multiple output files

--- pymiere/code_generation/extract_class_data.py
+++ pymiere/code_generation/extract_class_data.py
@@ -1,14 +1,32 @@
 from pprint import pprint
 from pymiere import Pymiere, utils
 
-filepath_jsxObjectToPythonObject = r"D:\code\prpro\jsxObjectToPythonObject.jsx"
+filepath = r"D:\code\prpro\pymiere\code_generation\jsxObjectToPythonObject.jsx"
 
+things_to_extract = [
+    ('$.global', "$"),
+    ('qe', "bababababa"),
+    ('app.project.rootItem.children[0].getFootageInterpretation()', "bababababa"),
+    ('app.project.rootItem.children[0].getOutPoint()', "bababababa"),
+    ('app.project.activeSequence.videoTracks[0]', "bababababa"),
+    ('app.project.activeSequence.videoTracks[0].clips', "bababababa"),
+    ('app.project.activeSequence.videoTracks[0].clips[0]', "bababababa"),
+    ('app.project.activeSequence.getSettings()', "bababababa"),
+    ('app.project.activeSequence.markers.getFirstMarker()', "bababababa"),
+    ('app.project.activeSequence.videoTracks[0].clips[0].components[0]', "bababababa"),
+    ('$', "global"),
+    ('Folder.current.getFiles("*.exe")', "bababababa"),
+    ('app.project.activeSequence.videoTracks[0].clips[0].components[0].properties[0]', "bababababa")
+]
 
 pymiere = Pymiere()
-data = pymiere.eval_script(filepath=filepath_jsxObjectToPythonObject)
-print("Result :")
-pprint(data, depth=3)
-
-utils.write_json_file(r"D:\code\prpro\classData_globals.json", data)
-
-# todo try using https://github.com/Adobe-CEP/Samples/tree/master/PProPanel/jsx PremierePro.d.ts
+for thing_to_extract in things_to_extract:
+    # using encoding 'utf-8-sig' to work with file saved with Adobe ExtendScript Toolkit
+    with open(filepath, encoding='utf-8-sig') as f:
+        code = f.read()
+    code = code.replace("__PLACEHOLDER__", '{}, "{}"'.format(thing_to_extract[0], thing_to_extract[1]))
+    data = pymiere.eval_script(code=code)
+    print("Result :")
+    pprint(data, depth=2)
+    filename = thing_to_extract[0].replace(".","").replace("[","").replace("]","").replace("(","").replace(")","").replace("*","").replace('"','')
+    utils.write_json_file(r"D:\code\prpro\pymiere\code_generation\class_datas\classDataExtract_{}.json".format(filename), data)

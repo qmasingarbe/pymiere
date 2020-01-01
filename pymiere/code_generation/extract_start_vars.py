a4bb2e6d07745df0cce8b8187f3286dafec6515b
Quentin Masingarbe <q.masingarbe@gmail.com>
2020-01-01 17:43:58 +0100

    Huge refactor, renamed classes and functions, delete Pymiere class Module hierarchy refactor

--- pymiere/code_generation/extract_start_vars.py
+++ pymiere/code_generation/extract_start_vars.py
@@ -1,8 +1,8 @@
-from pprint import pprint
-from pymiere import Pymiere, utils
 import os
+from pprint import pprint
+from pymiere import utils
+from pymiere.core import eval_script
 
-pymiere = Pymiere()
-data = pymiere.eval_script(filepath=os.path.join(__file__, "..", "jsxExtractStartVars.jsx"))
+data = eval_script(filepath=os.path.join(__file__, "..", "jsxExtractStartVars.jsx"))
 utils.write_json_file(os.path.join(__file__, "..", "class_datas", "start_vars.json"), data)
 pprint(data, depth=2)

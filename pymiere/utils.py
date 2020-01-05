9f811fd62ad22a157c6f9e769dfc825c13819e88
Quentin Masingarbe <q.masingarbe@gmail.com>
2020-01-05 17:35:43 +0100

    pep8

--- pymiere/utils.py
+++ pymiere/utils.py
@@ -2,20 +2,24 @@ import os, json
 
 TYPE_CORRESPONDENCE = {"string": "str", "boolean": "bool", "number": "float", "any": "any", "undefined": "None"}
 
+
 def write_json_file(filepath, data):
     if not os.path.isdir(os.path.dirname(filepath)):
         os.makedirs(os.path.dirname(filepath))
     with open(filepath, 'w') as f:
         json.dump(data, f, indent=2)
 
+
 def read_json_file(filepath):
     with open(filepath, 'r') as f:
         data = json.load(f)
     return data
 
+
 class MyStr(str):
     """Subclass of string to add code line quicker"""
     def add_line(self, content, indent=0):
         return MyStr(self + "\n" + indent*4*" " + content)
+
     def add_empty_line(self):
         return MyStr(self + "\n")

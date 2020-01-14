9cea5bf2b4d7a32d4887a808bd1bdceb77512457
Quentin Masingarbe <q.masingarbe@gmail.com>
2020-01-14 16:34:44 +0100

    Actual demo test code

--- test.py
+++ test.py
@@ -1,35 +1,18 @@
 import pymiere
-import pymiere.core
-import time
+from pymiere import wrappers
 
-get_sequence = """var sequence = app.project.activeSequence;
-var all_tracks = [];
-for(var i=0; i<sequence.videoTracks.numTracks; i++){
-    all_tracks.push(sequence.videoTracks[i]);
-}
-JSON.stringify(all_tracks)"""
+# check project is opened
+project_opened, sequence_active = wrappers.check_active_sequence(crash=False)
+if not project_opened:
+    raise ValueError("please open a project")
 
-if __name__== "__main__":
-    start_time = time.time()
-    sequence = pymiere.objects.app.project.activeSequence
-    print(sequence.name)
-    print(sequence.id)
-    sequence.name = "nom sympa"
-    try:
-        sequence.id = "caca"
-    except AttributeError:
-        print("Id is read only check ok")
-    print(sequence.getInPoint())
-    playerPos = sequence.getPlayerPosition()
-    print(playerPos)
-    print(playerPos.ticks)
+project = pymiere.objects.app.project
 
-    clip = sequence.videoTracks[0].clips[0]
-    new_time = pymiere.Time()
-    new_time.seconds = 5
-    sequence.setPlayerPosition(str(new_time.ticks))
+# open sequences if none active
+if not sequence_active:
+    sequences = wrappers.list_sequences()
+    for seq in sequences:
+        project.openSequence(sequenceID=seq.sequenceID)
+    project.activeSequence = sequences[0]
 
-    print(new_time.ticks)
-
-    sequence.setOutPoint(10)
-    print(time.time() - start_time)
+# list videos

2f19f9ddddc275b56a47836aa5546deecec84027
Peter Fison <peter@southwestlondon.tv>
2020-11-11 04:41:42 +0000

    Several suggestions to show off Pymiere features

--- demo_batch.py
+++ demo_batch.py
@@ -1,30 +1,53 @@
 import pymiere
 import pymiere.exe_utils as pymiere_exe
 import requests
+sjpwimport PySimpleGUI as sg
+from pathlib import Path
 
-# start premiere
-if pymiere_exe.exe_is_running("adobe premiere pro.exe")[0]:
-    raise ValueError("There already is a running instance of premiere")
-pymiere_exe.start_premiere()
+# Use PySimpleGui to select a .pproj file
+path = Path(sg.popup_get_file("Please select a Premiere Pro project to open", default_path="C:\\Users\\Quentin\\Desktop\\temp\\test.pproj", file_types=(("Premiere Pro", "*.prproj"),)))
 
-# open a project
-pymiere.objects.app.openDocument("C:\\Users\\Quentin\\Desktop\\temp\\test.pproj")
+# Start Premiere Pro and open the selected project
+if not pymiere_exe.exe_is_running("adobe premiere pro.exe")[0]:
+    pymiere_exe.start_premiere()
+    # TODO: Perhaps rename to just 'pymiere_exe.start'?  Mentioning premiere seems unnecessary...
+pymiere.objects.app.openDocument(str(path))
+# TODO: It would be nice for .openDocument to take a pathlib object itself (so you don't have to remember str(path).add()
+# TODO: It would also be nice to be able to start Premiere and open a Project all in one command by passing a Path argument e.g. pymiere_exe.start(path)
 
-# set sequence named hello world as active sequence
-sequences = pymiere.objects.app.project.sequences
-sequence = next(filter(lambda s: s.name == "hello world", sequences))
-pymiere.objects.app.project.openSequence(sequence.sequenceID)
-pymiere.objects.app.project.activeSequence = sequence
+# Open "hello world" and set it as the active sequence; if "hello world" doesn't exist, use the first Sequence found.
+project = pymiere.objects.app.project
+sequences = project.sequences
+# TODO: Can we remove wrappers.list_sequences() from demo.py? This seems easier and more readable!
+name = "hello world"
+sequence = list([s for s in sequences if s.name == name] or sequences)[0]
+project.openSequence(sequence.sequenceID)
+project.activeSequence = sequence
+# If there's any chance you might be changing the active sequence again, you can keep using 'project.activeSequence' to periodically check the CURRENT active sequence, otherwise the object stored as 'sequence' should do just fine.
 
-# rename sequence and query info
-pymiere.objects.app.project.activeSequence.name = "renamed sequence"
-print(pymiere.objects.app.project.activeSequence.getPlayerPosition().seconds)
-pymiere.objects.app.project.activeSequence.name = "hello world"  # rename back for test to work next time
-pymiere.objects.app.project.save()
+# Get some information about the Sequence.
+print("Current Sequence:", sequence.name)
+print("Resolution:", sequence.frameSizeHorizontal, " x", sequence.frameSizeVertical)
+print("Current Position:", round(sequence.getPlayerPosition().seconds, 3), "seconds")
+methods_and_attributes = [x for x in dir(sequence) if not x.startswith("_")]
+print("Other methods & attributes:")
+print("\n".join(methods_and_attributes))
 
-# close premiere
+# Backup, then batch-rename Sequences using the Project name as a prefix
+backup = {sequence: sequence.name for sequence in sequences}
+prefix = project.name.split(".prproj")[0]
+for sequence in sequences:
+    sequence.name = prefix + " - " + sequence.name
+
+# Restore Sequence names
+for sequence, name in backup.items():
+    sequence.name = name
+
+# Save the Project and Close Premiere Pro
+project.save()
 try:
     pymiere.objects.app.quit()
+    # TODO: Is there a way to override or preselect an option when the "Save project Yes/No" popup appears?
 except requests.exceptions.ConnectionError:
     print("Success")
     pass

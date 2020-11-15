16ab809344256774bbbf9ed704e293b856cc2844
Peter Fison <peter@southwestlondon.tv>
2020-11-15 06:32:06 +0000

    __version__ added to __init__.py

--- pymiere/__init__.py
+++ pymiere/__init__.py
@@ -1,3 +1,4 @@
 from pymiere.objects.premiere_objects import *
 from pymiere.objects.start_vars import StartVars
 objects = StartVars()  # entry point to the root level ExtendScript objects available
+from .pymiere import __version__
